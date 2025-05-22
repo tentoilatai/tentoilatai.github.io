2446b4e90fa1066d055f4459a28f0222de989111
Tai Tran Viet <iam@kaiz.vn>
2025-05-22 13:07:26 +0700

    ok

--- script.js
+++
@@ -1,8 +0,0 @@
-// Hàm để cập nhật năm hiện tại vào phần Copyright
-function updateCopyrightYear() {
-  var currentYear = new Date().getFullYear();
-  document.getElementById("copyright-year").textContent = currentYear;
-}
-
-// Gọi hàm khi trang được tải
-window.onload = updateCopyrightYear;
